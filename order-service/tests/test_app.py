9247abc118817a4bb4f5063e414f538ee75dd0bd
mystical_seeker02 <saksham.mishra2402@gmail.com>
2025-07-01 00:09:56 +0530

    Following modifications: Unit tests added for back-end endpoints Github Actions CI Pipeline Confirgured Linting, Type Checks, Tests, Docker Image Builds

---
+++ order-service/tests/test_app.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["services"] == "E-Commerce Order Service"
+    assert data["status"] == "operational"
+
+def test_health():
+    response = client.get("/v1/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
---
+++ product-service/tests/test_app.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Product Service is running."}
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
